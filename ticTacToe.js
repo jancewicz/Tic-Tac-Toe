e96440455818d40004cf45dee9b0a98492848655
jancewicz <jancewicz.pawel1@gmail.com>
2024-02-22 21:36:25 +0100

    search for appropriate search algorithm for 2d array

--- ticTacToe.js
+++ ticTacToe.js
@@ -1,39 +1,35 @@
-// gameboard as an array of GameBoard obcject
-
-// template for array of gameboard -> nested for loop
-// gameBoard = [
-//  [0, 0], [0, 1], [0, 2]
-//  [1, 0], [1, 1], [1, 2]
-//  [2, 0], [2, 1], [2, 2]
-//      ]
-
 function gameBoard() {
     const rows = 3;
     const columns = 3;
     const board = []
-
     for (let i = 0; i < rows; i++) {
         board[i] = [];
         for (let j = 0; j < columns; j++) {
             board[i].push(field);
         }
     }
-    console.log(board);
+    return board
 }
 
-
-function field() {
+function field(player) {
     let value = 0;
 
     const placePlayerMark = (player) => {
-        value = player
+        value = player.mark
     };
 
     function getValue() {
         return value;
     }
 
-    return { placePlayerMark, getValue };
+    function checkFieldsCurrentValue() {
+        // the way to read each of fields value (could be 0 -free, 1 - X's player mark, 2 - O's player mark)
+        board.forEach((row) => {
+            row.map((element) => console.log(field.getValue()));
+        });
+    }
+
+    return { placePlayerMark, getValue, checkFieldsCurrentValue };
 }
 
 // Players stored in objects -> Factory function for player with own name and functions like mark with X or O the field
@@ -58,34 +54,94 @@ function playGame() {
 
     let activePlayerIndex = 0;
 
-    function swapActivePlayer() {
-        //[players[0], players[1]] = [players[1], players[0]]; doesn't work with code below
-        activePlayerIndex = 1 - activePlayerIndex;
-    }
+    const activePlayerTurn = () => {
+        return players[activePlayerIndex];
+    };
 
-    return { players, playerTurn: () => players[activePlayerIndex], swapActivePlayer };
+    function swapActivePlayer() {
+        activePlayerIndex = 1 - activePlayerIndex;
+    };
+
+    return { players, activePlayerTurn, swapActivePlayer };
 }
 
 
 
-gameBoard();
+
+
+
+// playground 
+const board = gameBoard();
 
 const game = playGame();
-console.log(game.players)
-console.log(`TURN 1, active player is: ${game.playerTurn().name}`);
-
-console.log(`SWITCH`)
-game.swapActivePlayer();
-
-console.log(`TURN 2 active player after switch is: ${game.playerTurn().name}`)
-console.log(game.players)
 
 
-console.log(`switch number two!`);
-game.swapActivePlayer();
+const [playerOne, playerTwo] = game.players; // destructuring techique 
 
-console.log(`TURN 3 active player is: ${game.playerTurn().name}`);
-console.log(game.players);
+const gameField = field(playerOne);
+
+
+console.log(board)
+
+
+board.forEach((row) => {
+    row.map((element) => console.log(gameField.getValue()));
+});
+
+
+// What should happen when the turn is on: 
+// Choose field to place mark, 
+// check if field's value is 0 (free spot)
+// use field.placePlayerMark 
+// end turn
+console.log(`TURN 1, active player is: ${game.activePlayerTurn().name}`);
+let input = prompt(`${game.activePlayerTurn().name} which field would you like to take?`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(`TURN 1, active player is: ${game.playerTurn().name}`);
+
+// console.log(`SWITCH`)
+// game.swapActivePlayer();
+
+// console.log(`TURN 2 active player after switch is: ${game.playerTurn().name}`)
+// console.log(game.players)
+
+
+// console.log(`switch number two!`);
+// game.swapActivePlayer();
+
+// console.log(`TURN 3 active player is: ${game.playerTurn().name}`);
+// console.log(game.players);
+
+
+
 
 
 // Object to control the game itself: start the game with all fields clear, turns, win / lose / tie,
